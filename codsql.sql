d3aa7b5fd9bc06ffea31641e9aee477b1d113611
Pedro Henrique da Silva <32113151+p3dr0w@users.noreply.github.com>
2018-11-25 13:41:44 -0200

    Update codsql.sql

--- codsql.sql
+++ codsql.sql
@@ -8,7 +8,7 @@ CREATE TABLE `palestra` (
   `palestrante` varchar(100) NOT NULL,
   `del` bit(1) DEFAULT b'0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 
 CREATE TABLE `participante` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -20,13 +20,4 @@ CREATE TABLE `participante` (
   PRIMARY KEY (`id`),
   KEY `fk-palestra_idx` (`idpalestra`),
   CONSTRAINT `fk-palestra` FOREIGN KEY (`idpalestra`) REFERENCES `palestra` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
-
-INSERT INTO `palestra` (`id`, `nome`, `data`, `palestrante`, `del`) VALUES
-(9, 'BRAIN STORM', '2019-01-09 00:00:00', 'Luis Cunha Alves', b'0'),
-(10, 'PALESTRA MOTIVACIONAL', '2020-02-02 00:00:00', 'Diogo Pereira Azevedo', b'0');
-INSERT INTO `participante` (`id`, `nome`, `email`, `celular`, `idpalestra`, `del`) VALUES
-(1, 'Fábio Lima Araujo', 'fabio@live.com', '99999999999', 9, b'0'),
-(2, 'Luís Gomes Almeida', 'luis@live.com', '99999999999', 9, b'0'),
-(3, 'Thiago Souza Costa', 'thiago@hotmail.com', '99999999999', 10, b'0'),
-(4, 'Júlio Fernandes Ferreira', 'julio@outlook.com', '99999999999', 10, b'0');
+) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8;
